a27304572d00c79146c4ba6fb1417fc251220e61
eisbaer66 <janweidner87@gmail.com>
2022-05-14 15:37:18 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+using log4net.Config;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var logger = LogManager.GetLogger(typeof(Program));
+
+            try
+            {
+                logger.Error("TEST");
+            }
+            finally
+            {
+                LogManager.Flush(1000);
+            }
+
+        }
+    }
+}
